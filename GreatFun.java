3828123e0a4fa8877df1dabfab5fd2d26bb91e79
warapornsonpeng <152350329+warapornsonpeng@users.noreply.github.com>
2024-07-25 00:30:21 -0700

    Create GreatFun.java

---
+++ GreatFun.java
@@ -0,0 +1,7 @@
+public class GreatFun {
+    public static void main(String[] args){
+        System.out.print("Program is ");
+        System.out.println("Great Fun");
+        System.out.print("These are our top sellers: ");
+    }
+}
