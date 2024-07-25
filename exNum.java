29b8416f812ab1dbdf56c5120f39e6839616e618
warapornsonpeng <152350329+warapornsonpeng@users.noreply.github.com>
2024-07-25 00:30:17 -0700

    Create exNum.java

---
+++ exNum.java
@@ -0,0 +1,8 @@
+public class exNum {
+    public static void main(String[] args) {
+        int x = 62;
+        int y = 99;
+        int total = x + y;
+        System.out.println(" Num" +x+ " and" +y+ " is" +total);
+    }
+}
