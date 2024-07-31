c390eb84983cced88c04e64240732ba836932262
warapornsonpeng <152350329+warapornsonpeng@users.noreply.github.com>
2024-07-31 01:46:53 -0700

    Create TestFon.java

---
+++ TestFon.java
@@ -0,0 +1,8 @@
+public class TestFon {
+    public static void main(String[] args) {
+         for (int i = 1; i <= 10; i++) {
+            System.out.println("2 x " + i + " = " + (2 * i));
+    }
+}
+}
+
