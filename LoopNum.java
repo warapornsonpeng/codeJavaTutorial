b52f91d1eeb49dd000c5ec3a55111297d5d60530
warapornsonpeng <152350329+warapornsonpeng@users.noreply.github.com>
2024-07-31 01:46:46 -0700

    Create LoopNum.java

---
+++ LoopNum.java
@@ -0,0 +1,8 @@
+public class LoopNum {
+    public static void main(String[] args) {
+        for(int number = 1; number < 5; number++){
+            System.out.println(number+" ");
+        }
+        
+    }
+}
