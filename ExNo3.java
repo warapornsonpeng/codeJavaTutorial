94ae5f89046573cb50a7dfb9d6d9fbd8e50b88d3
warapornsonpeng <152350329+warapornsonpeng@users.noreply.github.com>
2024-08-01 00:36:02 -0700

    Create ExNo3.java

---
+++ ExNo3.java
@@ -0,0 +1,10 @@
+public class ExNo3 {
+    public static void main(String[] args) {
+        int multiplier = 8;
+        System.out.println(" multiplier " + multiplier + ":");
+        for (int i = 1; i <= 12; i++) {
+            int result = multiplier * i;
+            System.out.println(multiplier + " x " + i + " = " + result);
+        }
+    }
+}
