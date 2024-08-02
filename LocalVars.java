486bf535357e2a203aeb3e8e03dfc3cf2b942107
warapornsonpeng <152350329+warapornsonpeng@users.noreply.github.com>
2024-08-02 01:13:44 -0700

    Create LocalVars.java

---
+++ LocalVars.java
@@ -0,0 +1,14 @@
+public class LocalVars {
+    public static void main(String[] args) {
+        phuket();
+        rayong();
+    }
+    public static void phuket(){
+        int birds = 5000;
+        System.out.println("In phuket there are " + birds + " birds.");
+    }
+    public static void rayong(){
+        int birds = 3500;
+        System.out.println("In rayong there are " +birds+ " birds.");
+    }
+}
