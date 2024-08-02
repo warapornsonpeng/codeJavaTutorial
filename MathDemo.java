105c732582ae0a8cebb3cb2e70d5486ae684d8bc
warapornsonpeng <152350329+warapornsonpeng@users.noreply.github.com>
2024-08-01 22:59:23 -0700

    Create MathDemo.java

---
+++ MathDemo.java
@@ -0,0 +1,8 @@
+public class MathDemo {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(4, 2));
+        System.out.println(Math.pow(6.0, 3.0));
+        System.out.println(Math.sqrt(9.0));
+        System.out.println(Math.random());
+    }
+}
