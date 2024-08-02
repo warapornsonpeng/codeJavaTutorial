6358c4fdb6123a8b432791109aeb07ca5a441375
warapornsonpeng <152350329+warapornsonpeng@users.noreply.github.com>
2024-08-02 01:13:41 -0700

    Create Local2.java

---
+++ Local2.java
@@ -0,0 +1,12 @@
+public class Local2 {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("x = " +x);
+        one();
+        System.out.println("x = " +x);
+    }
+    public static void one(){
+        int x = 3;
+        System.out.println("x = " +x);
+    }
+}
