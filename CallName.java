f649795b05934b9475d24d87dddc8b3c4aee6be1
warapornsonpeng <152350329+warapornsonpeng@users.noreply.github.com>
2024-08-01 22:59:14 -0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,11 @@
+public class CallName {
+    public static void main(String[] args) {
+        System.out.println("MY NAME IS ");
+        for (int i = 1; i <= 10; i++){
+            displaymessage();
+        }
+    }
+public static void displaymessage(){
+    System.out.println("Waraporn Sonpeng");
+}
+}
